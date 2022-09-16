f48e4dd0f9d6da80bae75f5a164c5101f6f85223
hpearce <hw312312@dal.ca>
2022-09-16 18:52:33 -0300

    feat(test): Update test suite to pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
+    expect(compiled.querySelector('#construction')?.textContent).toContain('UNDER CONSTRUCTION');
   });
 });
