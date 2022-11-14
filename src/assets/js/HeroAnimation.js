c99fed4cc4026904f66d692fa938057af7ab5cc0
hpearce <hw312312@dal.ca>
2022-11-13 22:02:21 -0400

    refactor: Position tracking for hero animation
    Now handled by a java OOP class. Implementation for position is also
    much cleaner and resilient to error.

--- src/assets/js/HeroAnimation.js
+++ src/assets/js/HeroAnimation.js
@@ -1,38 +1,84 @@
- var currentColorIsDark = false;
- var currentColorIsLight = false;
+var currentColorIsDark = false;
+var currentColorIsLight = false;
 
- var animation = function () {
+// constants
+const CIRCLE_SPEED = 0.1;
+const DRIFT_SPEED = 0.05;
+const GUARD = 0.05;
+const SCENE_WIDTH = 14;
+const SCENE_HEIGHT = 22;
+
+class DotGrid {
+  constructor(screenWidth, sceneWidth, sceneHeight, screenHeight, particleSeparation) {
+    this.screenWidth = screenWidth;
+    this.sceneWidth = sceneWidth;
+    this.sceneHeight = sceneHeight;
+    this.screenHeight = screenHeight;
+    this.particleSeparation = particleSeparation;
+    this.particleCount = Math.round((this.sceneWidth / this.particleSeparation) * (this.sceneHeight / this.particleSeparation));
+    this.originalParticlePosition = new Float32Array(this.numberOfParticles * 3);
+    this.currentParticlePosition = new Float32Array(this.numberOfParticles * 3);
+    this.particleColors = new Float32Array(this.numberOfParticles * 3);
+  }
+
+  initializeParticles() {
+    var startX = this.sceneWidth / -2;
+    var startY = this.sceneHeight / 2;
+    var endX = this.sceneWidth / 2;
+    var endY = this.sceneWidth / -2;
+    var x = startX;
+    var y = startY;
+    var z = -5;
+    var index = 0;
+    // for every particle, set its position
+    for (let i = 0; i < this.numberOfRowsRequired; i+=1) {
+      for ( let j = 0; j < this.particlesPerRow; j+=1) {
+        //console.log(x.toFixed(2) + "," + y.toFixed(2));
+        this.currentParticlePosition[index] = x;
+        this.currentParticlePosition[index+1] = y;
+        this.currentParticlePosition[index+2] = z;
+        this.originalParticlePosition[index] = x;
+        this.originalParticlePosition[index+1] = y;
+        this.originalParticlePosition[index+2] = z;
+        x += this.particleSeparation;
+        index += 3;
+      }
+      y -= this.particleSeparation;
+      x = startX;
+    }
+  }
+
+  get particlesPerRow() {
+    return Math.round(this.sceneWidth / this.particleSeparation);
+  }
+
+  get numberOfRowsRequired() {
+    return Math.round(this.sceneHeight / this.particleSeparation);
+  }
+
+  get numberOfParticles() {
+    return this.particlesPerRow * this.numberOfRowsRequired;
+  }
+
+}
+
+var animation = function () {
   // container for animation
   const canvas = document.querySelector("#hero-graphic");
   var width = canvas.clientWidth;
   var height = canvas.clientHeight;
 
-  // constants
-  const CIRCLE_SPEED = 0.1;
-  const DRIFT_SPEED = 0.05;
-  const GUARD = 0.05;
-  const SCENE_WIDTH = 50;
-  const SCENE_HEIGHT = 10;
-  const NEGATIVE_BOUND_Y = -5;
-  const POSITIVE_BOUND_Y = NEGATIVE_BOUND_Y + SCENE_HEIGHT;
-  const NEGATIVE_BOUND_X = -25;
-  const POSITIVE_BOUND_X = NEGATIVE_BOUND_X + SCENE_WIDTH;
+  var grid = new DotGrid(width, SCENE_WIDTH, SCENE_HEIGHT, height, 0.125);
+  grid.initializeParticles();
 
-  // look at screen height. Change distance between each particle depending on screen size.
-  // linear function derived from points (1126, 0.65), (700, 0.1)
-  const getParticleSeparation = (inputHeight) => Math.max(0.04, inputHeight * -0.00008215962441314555 + 0.1575117370892018);
-  var PARTICLE_SEPARATION = getParticleSeparation(height);
+  console.log(grid);
+  console.log("Particles per row: " + grid.particlesPerRow);
+  console.log("Number of rows required: " + grid.numberOfRowsRequired);
 
-  // FIXME: Multiplying by 1.3 since rounding causes us to come up just a little bit short on the number of particles required.
-  var particleCount = Math.round((SCENE_WIDTH / PARTICLE_SEPARATION) * (SCENE_HEIGHT / PARTICLE_SEPARATION) * 1.3);
   var waveCenterX = 0;
   var waveCenterY = 0;
   var radius = 0.1;
 
-  // the original position of every particle in the array
-  const originalParticlePosition = new Float32Array(particleCount * 3);
-  // the current position (after being moved)
-  var currentParticlePosition = new Float32Array(particleCount * 3);
   // color of each particle
   var particleColors = [];
   var darkParticleColors = [];
@@ -46,7 +92,7 @@
 
   // set up scene
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 9999 );
+  const camera = new THREE.PerspectiveCamera(115, width / height, 0.1, 9999 );
   const renderer = new THREE.WebGLRenderer( { alpha: true } );
 
   configureScene();
@@ -58,9 +104,6 @@
     renderer.render(scene, camera);
   });
 
-  console.log(bufferGeometry);
-  console.log(particleCount);
-
   animate();
 
   /* FUNCTION DECLARATIONS DOWN BELOW ------------------------------------------------------------- */
@@ -68,48 +111,29 @@
   function createParticleArray() {
     // initialize array of dots
     const material = new THREE.PointsMaterial( { size: 0.01, vertexColors: true } );
-
-    // populate array with values (create a rectangular grid)
-    var x = NEGATIVE_BOUND_X;
-    var y = POSITIVE_BOUND_Y;
-    var z = -5;
-    // for every particle, set its position
-    for (let i = 0; i < particleCount * 3; i+=3) {
-      currentParticlePosition[i] = x;
-      currentParticlePosition[i+1] = y;
-      currentParticlePosition[i+2] = z;
-      originalParticlePosition[i] = x;
-      originalParticlePosition[i+1] = y;
-      originalParticlePosition[i+2] = z;
-      // wrap around at end of line
-      if (x > POSITIVE_BOUND_X) {
-        x = NEGATIVE_BOUND_X;
-        y -= PARTICLE_SEPARATION;
-      } else {
-        x += PARTICLE_SEPARATION;
-      }
-    }
-
-    bufferGeometry.setAttribute('position', new THREE.BufferAttribute(currentParticlePosition, 3));
+    bufferGeometry.setAttribute('position', new THREE.BufferAttribute(grid.currentParticlePosition, 3));
     bufferGeometry.dynamic = true;
     var particlesMesh = new THREE.Points(bufferGeometry, material);
     scene.add(particlesMesh);
   }
 
   function createColorArray(color) {
-    const particleColor = new THREE.Color(color);
-    for (let i = 0; i < particleCount; i+= 1) {
-      particleColor.setHex(color);
+    //colors = [0xEC7063, 0x884EA0, 0x5DADE2, 0x76D7C4, 0x58D68D, 0xF7DC6F, 0xF8C471, 0xF0B27A ];
+    //j = 0;
+    const particleColor = new THREE.Color(0xffffff);
+    for (let i = 0; i < grid.numberOfParticles; i+= 1) {
+
+      particleColor.setHex(0xffffff);
       particleColors.push(particleColor.r, particleColor.g, particleColor.b)
+
     }
     bufferGeometry.setAttribute('color', new THREE.Float32BufferAttribute(particleColors, 3));
   }
 
   function changeColor(color) {
-    console.log("Switching to color " + color);
     const colorToChangeTo = new THREE.Color(color);
     var colorArray = bufferGeometry.attributes.color.array;
-    for (let i = 0; i < particleCount * 3; i += 3) {
+    for (let i = 0; i < grid.numberOfParticles * 3; i += 3) {
       colorArray[i] = colorToChangeTo.r;
       colorArray[i+1] = colorToChangeTo.g;
       colorArray[i+2] = colorToChangeTo.b;
@@ -142,17 +166,17 @@
         position[index] += DRIFT_SPEED;
       }
     } else {
-      position[index] = originalParticlePosition[index];
+      position[index] = grid.originalParticlePosition[index];
     }
   }
 
   function animateParticles() {
-    for (let i = 0; i < particleCount * 3; i+=3) {
+    for (let i = 0; i < grid.numberOfParticles * 3; i+=3) {
       var currentPosition = bufferGeometry.attributes.position.array;
       // calculate the difference between its origin and current location in all 3 dimensions
-      var diffX = originalParticlePosition[i] - currentPosition[i];
-      var diffY = originalParticlePosition[i+1] - currentPosition[i+1];
-      var diffZ = originalParticlePosition[i+2] - currentPosition[i+2];
+      var diffX = grid.originalParticlePosition[i] - currentPosition[i];
+      var diffY = grid.originalParticlePosition[i+1] - currentPosition[i+1];
+      var diffZ = grid.originalParticlePosition[i+2] - currentPosition[i+2];
       driftToOrigin(diffX, i, currentPosition);
       driftToOrigin(diffY, i+1, currentPosition);
       driftToOrigin(diffZ, i+2, currentPosition);
@@ -165,11 +189,11 @@
     radius += CIRCLE_SPEED;
     var currentPosition = bufferGeometry.attributes.position.array;
     //bufferGeometry.material.color.setHex(0xff0000);
-    for (let i = 0; i < particleCount * 3; i+=3) {
+    for (let i = 0; i < grid.numberOfParticles * 3; i+=3) {
       var distance = distanceFromCenter(currentPosition[i], currentPosition[i+1]);
       var normalizedDistance = radius - distance;
       if ( Math.abs(normalizedDistance) < 0.8 ) {
-        currentPosition[i+2] = originalParticlePosition[i+2] + (Math.cos(normalizedDistance * 2) / 2);
+        currentPosition[i+2] = grid.originalParticlePosition[i+2] + (Math.cos(normalizedDistance * 2) / 2);
       }
     }
 
