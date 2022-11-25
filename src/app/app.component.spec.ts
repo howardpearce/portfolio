cdfa02a667d271e6e9ca66250af28fc8d43e199e
hpearce <hw312312@dal.ca>
2022-11-25 15:46:02 -0400

    feat: Add animation for preloader

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from './structural-components/footer/footer.component
 
 import { ColorSwitcherComponent } from './utility-components/color-switcher/color-switcher.component';
 import { PlusSignComponent } from './utility-components/plus-sign/plus-sign.component';
-
+import { PreloaderComponent } from './utility-components/preloader/preloader.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
